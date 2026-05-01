e3c7b9ead72d7e3810804d20d484019cd0e96bd8
Mubinul Islam Mushfique <mubinulislammushfique@gmail.com>
2026-05-01 23:29:02 +0600

    Create Assesment_01.java

---
+++ Assesment_01.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Assesment_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-- Welcome to Howard Shop --");
+
+        System.out.print("Enter Product Name: ");
+        String productName = sc.nextLine();
+
+        System.out.print("Enter Unit Price: ");
+        double unitPrice = sc.nextDouble();
+
+        System.out.print("Enter Quantity: ");
+        int quantity = sc.nextInt();
+
+        System.out.print("Enter Discount Amount in Percentage: ");
+        double discount = sc.nextDouble();
+
+        double DISCOUNT_RATE = discount / 100;
+
+        double subtotal = unitPrice * quantity;
+        double discountAmount = subtotal * DISCOUNT_RATE;
+        double finalPrice = subtotal - discountAmount;
+
+        int roundedFinalPrice = (int) finalPrice;
+
+        System.out.println("\n--- Final Receipt ---");
+        System.out.println("Item: " + productName);
+        System.out.println("Subtotal: $" + subtotal);
+        System.out.println("Discount Applied: $" + discountAmount);
+        System.out.println("Total (Rounded Down): $" + roundedFinalPrice);
+        System.out.println("--------------------");
+
+        sc.close();
+    }
+}
